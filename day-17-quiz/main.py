c14f9177defba1ca579fec5d5a0a7e0ae930fa96
Amy Gurski <amygurski@gmail.com>
2021-09-27 16:10:23 -0400

    day 17 add quiz and ask player next question

--- day-17-quiz/main.py
+++ day-17-quiz/main.py
@@ -1,7 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
     question_bank.append(Question(question["text"], question["answer"]))
+
+q = QuizBrain(question_bank)
+q.next_question()
+
 
