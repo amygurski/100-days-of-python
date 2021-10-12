515c520c5027c26c90c43eb02fcc3bf37f16dcf0
Amy Gurski <amygurski@gmail.com>
2021-10-12 15:46:15 -0400

    day 22 add ball and scoreboard and finish game

--- day-22-pong/main.py
+++ day-22-pong/main.py
@@ -1,9 +1,17 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 SCREEN_WIDTH = 800
+SCREEN_HORIZONTAL_EDGE = (SCREEN_WIDTH / 2) - 20
+
 SCREEN_HEIGHT = 600
-PADDLE_POSITION = (SCREEN_WIDTH/2)-50
+SCREEN_VERTICAL_EDGE = (SCREEN_HEIGHT / 2) - 20
+
+PADDLE_POSITION = (SCREEN_WIDTH / 2)-50
+PADDLE_BALL_CONTACT = (SCREEN_WIDTH / 2) - 80
 
 # Setup screen
 screen = Screen()
@@ -14,6 +22,8 @@ screen.tracer(0)
 
 right_paddle = Paddle((PADDLE_POSITION,0))
 left_paddle = Paddle((-PADDLE_POSITION,0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(right_paddle.go_up, "Up")
@@ -23,6 +33,32 @@ screen.onkey(left_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > SCREEN_VERTICAL_EDGE or ball.ycor() < -SCREEN_VERTICAL_EDGE:
+        ball.bounce_y()
+
+    # Detect collision with paddle 
+    if (ball.distance(right_paddle) < 50 and ball.xcor() > PADDLE_BALL_CONTACT) or (ball.distance(left_paddle) < 50 and ball.xcor() < -PADDLE_BALL_CONTACT):
+        ball.bounce_x()
+        ball.move_speed *= 0.9
+
+    # Detect Right paddle miss
+    if ball.xcor() > SCREEN_HORIZONTAL_EDGE:
+        ball.reset_position()
+        scoreboard.l_point()
+    
+    # Detect Left paddle miss
+    if ball.xcor() < -SCREEN_HORIZONTAL_EDGE:
+        ball.reset_position()
+        scoreboard.r_point()
+    
+    # End game when someone has 5 points
+    if scoreboard.l_score >= 5 or scoreboard.r_score >= 5:
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
