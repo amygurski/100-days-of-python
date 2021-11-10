f80bc0ef2640e4b0597df27833912366d651b150
Amy Gurski <amygurski@gmail.com>
2021-11-10 15:28:14 -0500

    add error handling to nato alphabet

--- day-26-nato-alphabet-project/main.py
+++ day-26-nato-alphabet-project/main.py
@@ -5,8 +5,15 @@ df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (i,row) in df.iterrows()}
 
 print("Hi! I'll tell you the phonetic spelling of a word.")
-word = input("Enter a word: ")
 
-nato_spelling = [nato_dict[letter.capitalize()] for letter in word]
+def generate_phonetic_alphabet():
+    word = input("Enter a word: ")
+    try:
+        nato_spelling = [nato_dict[letter.capitalize()] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the English alphabet please.")
+        generate_phonetic_alphabet()
+    else:
+        print(nato_spelling)
 
-print(nato_spelling)
+generate_phonetic_alphabet()
