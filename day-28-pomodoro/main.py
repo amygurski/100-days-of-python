29365fce820e9e396c56ad45159d2d46778792a8
Amy Gurski <amygurski@gmail.com>
2021-11-24 12:16:42 -0500

    fix pomodoro title

--- day-28-pomodoro/main.py
+++ day-28-pomodoro/main.py
@@ -56,7 +56,7 @@ def count_down(count):
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title = ("Pomodoro")
+window.title("Pomodoro")
 window.config(padx=100,pady=50, bg=YELLOW)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
